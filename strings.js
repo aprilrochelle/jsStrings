882855d867b3f0d143dd4c36611ee963241037cd
April Nichols <nicholsar@gmail.com>
2018-02-26 20:30:44 -0600

    2nd challenge completed

--- strings.js
+++ strings.js
@@ -2,3 +2,9 @@
 
 var numOfSandwiches = 8;
 console.log("numOfSandwiches", numOfSandwiches);
+
+// Challenge #2
+
+var name = "April";
+var string = "Hello " + name + "! How are you doing today?";
+console.log("string:", string);
